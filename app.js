2e7457bd718b50b4b9eb88cf251a084b5c30742b
pskumar7145 <pskumar7145@gmail.com>
2026-05-25 16:04:36 +0800

    deploy v2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("✅ 🚀 CI/CD is working LIVE!");
+    res.send("🔥 Version v2 deployed!");
 });
 
 app.get('/health', (req, res) => {
