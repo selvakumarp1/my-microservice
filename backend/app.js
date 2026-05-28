572b8c1d6ee31e9fe0ae19cbd474395f23dc900b
pskumar7145 <pskumar7145@gmail.com>
2026-05-28 15:11:42 +0800

    Trigger dev deployment

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 
 // ✅ API route (IMPORTANT for Ingress /api)
 app.get('/api', (req, res) => {
-  res.send("🔥 CI/CD WORKING v2 🚀");
+  res.send("🔥🧪 DEV ENVIRONMENT 🚀 🚀");
 });
 
 // ✅ Start server
